c5a16d7547e3d499ead9411d359b6210671311aa
BKMMYTG <yilmaz.murat@tcmb.gov.tr>
2021-01-20 22:06:02 +0300

    BeanUtils.copy properties

--- src/main/java/com/bluegiant/task/util/ConverterUtil.java
+++ src/main/java/com/bluegiant/task/util/ConverterUtil.java
@@ -2,31 +2,20 @@ package com.bluegiant.task.util;
 
 import com.bluegiant.task.dto.FormDataDto;
 import com.bluegiant.task.model.FormData;
+import org.springframework.beans.BeanUtils;
 
 public class ConverterUtil {
 
     public static FormData toFormData(FormDataDto formDataDto) {
         FormData formData = new FormData();
-        formData.setId(formDataDto.getId());
-        formData.setAd(trimIfNecessary(formDataDto.getAd()));
-        formData.setSoyad(trimIfNecessary(formDataDto.getSoyad()));
-        formData.setAdres(trimIfNecessary(formDataDto.getAdres()));
-        formData.setDogumYeri(trimIfNecessary(formDataDto.getDogumYeri()));
-
+        BeanUtils.copyProperties(formDataDto, formData);
         return formData;
     }
 
     public static FormDataDto toFormDataDto(FormData formData) {
         FormDataDto formDataDto = new FormDataDto();
-        formDataDto.setId(formData.getId());
-        formDataDto.setAd(trimIfNecessary(formData.getAd()));
-        formDataDto.setSoyad(trimIfNecessary(formData.getSoyad()));
-        formDataDto.setAdres(trimIfNecessary(formData.getAdres()));
-        formDataDto.setDogumYeri(trimIfNecessary(formData.getDogumYeri()));
+        BeanUtils.copyProperties(formData, formDataDto);
         return formDataDto;
     }
 
-    private static String trimIfNecessary(String txt) {
-        return txt != null ? txt.trim() : "";
-    }
 }
