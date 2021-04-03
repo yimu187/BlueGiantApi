42e4ae59a42e6663efc0490355d02ee62b09b5aa
BKMMYTG <yilmaz.murat@tcmb.gov.tr>
2021-04-04 00:34:28 +0300

    1.0.7

---
+++ src/main/resources/static/precache-manifest.5df6b4ba7ef419e42257997acbee1053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2755964f06d77f7d4185",
+    "url": "/static/css/main.88d5f900.chunk.css"
+  },
+  {
+    "revision": "2755964f06d77f7d4185",
+    "url": "/static/js/main.8e1fc9e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d2a6905b006249df8ed4",
+    "url": "/static/css/2.9e5a1fb5.chunk.css"
+  },
+  {
+    "revision": "d2a6905b006249df8ed4",
+    "url": "/static/js/2.c4ba5f29.chunk.js"
+  },
+  {
+    "revision": "520d7aaaf139104addc3d2a73fd90b20",
+    "url": "/index.html"
+  }
+];
